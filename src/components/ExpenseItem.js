5e0c57e3c1c4415e555c2240bafd6a2f03c4e8a3
Anamika Bajpai <anamikaagnihotri12@gmail.com>
2023-07-13 20:16:02 +0530

    Delete button functionality added(Task-1) in expense app

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,14 +3,25 @@ import ExpenseDetails from './ExpenseDetails'
  import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  console.log(props);
+ //function clickHandler() {}
+
+ const clickHandler = () => {
+  const expenseItem= document.querySelector('.expense-item');
+  expenseItem.remove();
+  console.log('Clicked!!!');
+ };
+
 
   return (
 
  <div className='expense-item'>
   <ExpenseDate date = {props.date} />
  
-      <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Delete Expense</button>
      
     </div>
  
